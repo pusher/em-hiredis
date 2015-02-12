ec57d3ea8e3154fc6a06d96e995d02de1c4862b4
Michael Pye <mail@mdpye.co.uk>
2015-02-12 16:10:01 +0000

    connect to 9 for tests

--- spec/support/connection_helper.rb
+++ spec/support/connection_helper.rb
@@ -1,10 +1,11 @@
 module ConnectionHelper
   # Use db 9 for tests to avoid flushing the main db
   # It would be nice if there was a standard db number for testing...
-  def connect(timeout = 1, uri = nil)
+  def connect(timeout = 1, uri = 'redis://localhost:6379/9')
     em(timeout) {
-      redis = EM::Hiredis.connect(uri)
-      redis.flushdb
+      redis = EM::Hiredis.connect(uri).callback {
+        redis.flushdb
+      }
       yield redis
     }
   end
