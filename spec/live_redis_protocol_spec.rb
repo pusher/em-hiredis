70dd5afe4e9e6fa6bea332a29e5a920086978de8
Michael Pye <mail@mdpye.co.uk>
2015-02-17 09:39:10 +0000

    Fix tests which fiddle with internals of the client

--- spec/live_redis_protocol_spec.rb
+++ spec/live_redis_protocol_spec.rb
@@ -496,7 +496,9 @@ describe EventMachine::Hiredis, "when reconnecting" do
   it "select previously selected dataset" do
     connect(3) do |redis|
       #simulate disconnect
-      redis.set('foo', 'a') { redis.close_connection_after_writing }
+      redis.set('foo', 'a') {
+        redis.instance_variable_get(:@connection).close_connection_after_writing
+      }
 
       EventMachine.add_timer(2) do
         redis.get('foo') do |r|
@@ -520,7 +522,7 @@ describe EventMachine::Hiredis, "when closing_connection" do
         op.callback { fail }
         op.errback { done }
 
-        redis.close_connection
+        redis.instance_variable_get(:@connection).close_connection
       }
       EM.add_timer(1) { fail }
     end
